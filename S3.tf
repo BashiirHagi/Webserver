cc32f73d53f568f5635a7fc76749a49463b49c65
Bashiir Hagi <bashiir854@gmail.com>
2022-01-14 16:45:17 +0000

    S3 bucket code

--- S3.tf
+++ S3.tf
@@ -1,30 +1,8 @@
 resource "aws_s3_bucket" "testbucket1-00" {
-  bucket = "my-tf-test-bucket-bashiir" # name of bucket
+  bucket = "my-tf-test-bucket-bashiir"  # name of bucket
   acl    = "private"
 
   tags = {
     Name = "bucket-311"
   }
 }
-
-//resource "aws_s3_bucket" "b" {
-// bucket = "s3-website-test.hashicorp.com"
-// acl    = "public-read"
-//policy = file("policy.json")
-
-//website {
-//index_document = "index.html"
-//error_document = "tech.jpeg"
-
-//routing_rules = <<EOF
-//[{
-//"Condition": {
-//"KeyPrefixEquals": "docs/"
-//},
-//"Redirect": {
-// "ReplaceKeyPrefixWith": "documents/"
-//}
-//}]
-//EOF
-//}
-//}
